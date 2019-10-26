b91e7098848fde09473506cc60e62478811007e5
materialcoder <1028106985@qq.com>
2019-10-26 22:59:25 +0800

    实现评论功能

---
+++ miniprogram/components/blog-ctrl/blog-ctrl.js
@@ -0,0 +1,113 @@
+// components/blog-ctrl/blog-ctrl.js
+let userInfo = {}
+const db = wx.cloud.database()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    blogId: String
+  },
+
+  externalClasses: ['iconfont', 'icon-pinglun', 'icon-fenxiang'],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // 当前登录组件是否显示
+    loginShow: false,
+    // 底部弹出层是否显示
+    modalShow: false,
+    content: '',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onComment() {
+      // 判断用户是否授权
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: (res) => {
+                userInfo = res.userInfo
+                // 显示评论弹出层
+                this.setData({
+                  modalShow: true
+                })
+              }
+            })
+          } else {
+            this.setData({
+              loginShow: true
+            })
+          }
+        }
+      })
+    },
+
+    onSend() {
+      // 插入云数据库
+      let content = this.data.content
+      if (content.trim() == '') {
+        wx.showModal({
+          title: '评论内容不能为空',
+          content: '',
+        })
+        return
+      }
+      wx.showLoading({
+        title: '评价中',
+        mask: true,
+      })
+      db.collection('blog-comment').add({
+        data: {
+          content,
+          createTime: db.serverDate(),
+          blogId: this.properties.blogId,
+          nickName: userInfo.nickName,
+          avatarUrl: userInfo.avatarUrl
+        }
+      }).then((res) => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '评论成功',
+        })
+        this.setData({
+          modalShow: false,
+          content: '',
+        })
+      })
+
+      // 推送模板消息
+    },
+
+    onInput(event) {
+      this.setData({
+        content: event.detail.value
+      })
+    },
+
+    onLoginSuccess(event) {
+      userInfo = event.detail
+      // 授权框消失，评论框显示
+      this.setData({
+        loginShow: false
+      }, () => {
+        this.setData({
+          modalShow: true
+        })
+      })
+    },
+
+    onLoginFail() {
+      wx.showModal({
+        title: '授权用户才能进行评价',
+        content: '',
+      })
+    }
+  }
+})
